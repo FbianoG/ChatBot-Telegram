adcfb76b6edf34a7d3aa3553e7b57681e60ab930
Fabiano Gonçalves <boriscasoy123@gmail.com>
2024-07-22 17:29:02 -0300

    update channels.js

--- public/utils/channels.js
+++ public/utils/channels.js
@@ -6,9 +6,39 @@ const channels = [
     },
     {
         name: 'ChatRecebe',
-        id: '2134377722'
-    }
+        id: '2134377722',
+        active: false,
+    },
+    {
+        name: 'Dupla Futebol ⚽',
+        id: '1214308482',
+        active: true,
+    },
+    {
+        name: 'Dupla Basquete 🏀',
+        id: '1216091692',
+        active: true,
+    },
+    {
+        name: 'Dupla FIFA 🎮',
+        id: '1355848104',
+        active: false,
+    },
+    {
+        name: 'Dupla Galgos 🐕‍🦺',
+        id: '1270361246',
+        active: false,
+    },
+    {
+        name: 'Dupla Cantos ⚽',
+        id: '1221399350',
+        active: true,
+    },
 ]
+
+function channelActive(id) {
+    return channels.some(element => element.id == id && element.active)
+}
 
 // Function find the channel ID
 function findChannel(id) {
@@ -18,4 +48,4 @@ function findChannel(id) {
     return channel.name
 }
 
-module.exports = {findChannel}
+module.exports = { findChannel, channelActive }
